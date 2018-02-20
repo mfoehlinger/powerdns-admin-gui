e004b156bda0535eec824e52879a53c3cfdd2fe4
Marcus Föhlinger <m.foehlinger@gmail.com>
2018-02-20 15:45:18 +0100

    [ADD] index.php

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the PowerDNS-Admin-GUI project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ */
+
+define('BASEDIR',__DIR__.DIRECTORY_SEPARATOR.'..');
+
+define('TPLDIR', 'template'.DIRECTORY_SEPARATOR.'default'.DIRECTORY_SEPARATOR);
+
